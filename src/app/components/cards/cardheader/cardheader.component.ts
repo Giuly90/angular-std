ff74653cf927b9ee95564fd07266407c36fcc625
Giuly90 <rosso.giulia.90@gmail.com>
2019-12-09 17:36:34 +0100

    1 file modified, 2 added

---
+++ src/app/components/cards/cardheader/cardheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardheader',
+  templateUrl: './cardheader.component.html',
+  styleUrls: ['./cardheader.component.css']
+})
+export class CardheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
