465c7fd2111fb9d03cda15d1e8e406fd4291f108
Giuly90 <rosso.giulia.90@gmail.com>
2019-12-10 12:33:40 +0100

    update employee-details component

--- src/app/components/employee-details/employee-details.component.ts
+++ src/app/components/employee-details/employee-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { EmployeeListComponent } from '../employee-list/employee-list.component';
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  employee: Employee;
+
+  constructor(private route: ActivatedRoute,private router: Router, private employeeService: EmployeeService) { }
 
   ngOnInit() {
+    this.employee = new Employee();
+    this.id = this.route.snapshot.params['id'];   
+    this.employeeService.getEmployee(this.id).subscribe(data => {
+        console.log(data)
+        this.employee = data;
+      }, error => console.log(error));
+  }
+
+  list(){
+    this.router.navigate(['employees']);
   }
 
 }
