118b4dc2d1b35d79de49bdcbc3ed61f9405b1878
Giuly90 <rosso.giulia.90@gmail.com>
2019-12-10 10:08:43 +0100

    4 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from "@angular/forms";
 import { CardsComponent } from "./components/cards/cards.component";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
+import { EmployeeService } from './services/employee.service';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, CardsComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HelloComponent, CardsComponent, CreateEmployeeComponent, EmployeeListComponent, EmployeeDetailsComponent],
+  bootstrap: [AppComponent],
+  providers: [EmployeeService]
 })
 export class AppModule {}
---
+++ src/app/components/employee-list/employee-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
+import { Observable } from "rxjs";
+import { EmployeeService } from "../employee.service";
+import { Employee } from "../employee";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
